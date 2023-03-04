903ed2c3b923d00e5711c0973a0073239ae5ec3d
Kevser Saruhan <kevser.saruhan@gmail.com>
2023-03-05 00:50:44 +0100

    second commit

--- src/test/java/baseurl/TrelloBaseUrl.java
+++ src/test/java/baseurl/TrelloBaseUrl.java
@@ -5,9 +5,19 @@ import utilities.ConfigReader;
 
 public class TrelloBaseUrl {
 
-    public static RequestSpecification spec;
-    public static void trelloSetUp(){
-        spec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("trello_url")).build();
+    public static RequestSpecification boardSpec;
+    public static RequestSpecification listSpec;
+    public static RequestSpecification cardSpec;
+
+    public static void boardSetUp(){
+        boardSpec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("board_url")).build();
+    }
+    public static void listSetUp(){
+        listSpec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("lists_url")).build();
+    }
+
+    public static void cardSetUp(){
+        cardSpec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("cards_url")).build();
     }
 
 }
